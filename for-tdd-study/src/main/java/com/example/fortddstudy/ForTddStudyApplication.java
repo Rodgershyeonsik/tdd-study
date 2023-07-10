bed36eb2df78442fe0fdad12169dda71f5c0a384
HEEJI YUN <yhj8147e@gmail.com>
2023-07-10 20:36:34 +0900

    init: tdd 스터디 프로젝트 초기화

---
+++ for-tdd-study/src/main/java/com/example/fortddstudy/ForTddStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fortddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForTddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForTddStudyApplication.class, args);
+    }
+
+}
